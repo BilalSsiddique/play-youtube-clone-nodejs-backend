7536e39ec55a291015b2cdfec6965ba6acc3798b
BilalSsiddique <bsiddiqui@students.uit.edu>
2024-02-03 21:55:38 +0500

    update multer filename

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,8 +5,8 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
   },
 });
 
