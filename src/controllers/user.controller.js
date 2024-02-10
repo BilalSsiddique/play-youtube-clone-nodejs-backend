0b0fb1c711e1b8226a1adf2e0cd4cc6fe8a8529e
BilalSsiddique <bsiddiqui@students.uit.edu>
2024-02-10 22:14:53 +0500

    delete temp files

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,15 +6,17 @@ import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  //get user details from frontend
-  //validation - not empty
-  // check if user already exists : username,email
-  // check for images, check for avatar
-  // upload them to cloudinary, : check if avatar is uploaded or not
-  // now create user object - create entry in db
-  // remove password and refresh token field from response
-  // check for user creation : if created or not
-  // return response
+  // REQUIREMENTS
+  /*get user details from frontend
+  validation - not empty
+  check if user already exists : username,email
+  check for images, check for avatar
+  upload them to cloudinary, : check if avatar is uploaded or not
+  now create user object - create entry in db
+  remove password and refresh token field from response
+  check for user creation : if created or not
+  return response
+  */
 
   const { username, fullName, email, password } = req.body;
 
